f130a6e101466aebb0d2dbbf8080ac8f9712ccca
FURQAAN194 <furqansaira91@gmail.com>
2025-05-21 19:28:15 +0530

    Add files via upload

---
+++ sum_integers_1_to_50.py
@@ -0,0 +1,12 @@
+
+# Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+# Initialize sum variable
+total_sum = 0
+
+# Loop from 1 to 50
+for number in range(1, 51):
+    total_sum += number
+
+# Display the final sum
+print("The sum of integers from 1 to 50 is:", total_sum)
